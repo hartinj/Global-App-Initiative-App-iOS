165ea47ee25cc6a2c8b6f894b9ce3ba26c5d65b3
John Hartinger <johnhartinger@wireless1x-155-41-80-61.bu.edu>
2014-10-23 19:57:39 -0400

    Second commit: Xcode project file

---
+++ GAI-App-iOS/GAI-App-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GAI-App-iOS
+//
+//  Created by John Hartinger on 10/23/14.
+//  Copyright (c) 2014 Global App Initiative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
